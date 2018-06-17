6f033c77f9992f10814618bb2e7f5332074a7ad6
daty.danghuynh <daty.danghuynh20497@gmail.com>
2018-06-17 23:15:31 +0700

    first commit

---
+++ AngularMVC/Controllers/BaseAPIController.cs
@@ -0,0 +1,21 @@
+﻿using AngularMVC.DBContext;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace AngularMVC.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
